985a420bb26ef668a005099a05aa8fa42a72d733
bhargavdeore <deorebhargav@gmail.com>
2024-03-30 13:41:20 +0530

    small changes

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -1,55 +1,36 @@
-import React, { useState } from 'react';
-import Recommended from './Recommended';
+import React, { useState } from "react";
+import Recommended from "./Recommended";
 
-import Aside from './aside';
-import './filter.css'
-import ProductCards from './ProductCards';
-
-
+import Aside from "./aside";
+import "./filter.css";
+import ProductCards from "./ProductCards";
 
 function DropdownMenu() {
-  
- 
-  
+  const [showFilter, setShowData] = useState(false);
 
-  
-  const [showData, setShowData]=useState(false)
-
-
-const toggleData =()=>{
-
-  setShowData(prevState => !prevState);
-  
-}
+  const toggleData = () => {
+    setShowData((prevState) => !prevState);
+  };
 
   return (
-   <>
-    <div className='app'>
-      <a href="#" onClick={toggleData} className='ms-5 text-secondary '>
-      {showData ? "Hide Data" : "Show Data"} 
-      </a> 
-      <Recommended/>
-      <hr />
-      <br />
-      <div className='contain'>
-      {
-        showData && <Aside/>
-      }
-      <div className={showData? 'content with-aside' : 'content'}>
-        <ProductCards/>
+    <>
+      <div className="app">
+        <span style={{ fontWeight: "bold" }}>3645</span>
+        <a href="#" onClick={toggleData} className="ms-5 text-secondary ">
+          {showFilter ? "Hide Filter" : "Show Filter"}
+        </a>
+        <Recommended />
+        <hr />
+        <br />
+        <div className="contain">
+          {showFilter && <Aside />}
+          <div className={showFilter ? "content with-aside" : "content"}>
+            <ProductCards />
+          </div>
         </div>
-      
       </div>
-      
-    </div>
-   </>
+    </>
   );
 }
 
-
 export default DropdownMenu;
-
-
-
-
-
